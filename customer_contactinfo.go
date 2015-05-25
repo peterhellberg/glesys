6cc4e80b872a7285b3488394620eee0b402d1431
Peter Hellberg <peter@c7.se>
2015-05-26 00:36:39 +0200

    Updated CustomerContactInfo type, variadic args

--- customer_contactinfo.go
+++ customer_contactinfo.go
@@ -8,15 +8,39 @@ type CustomerContactInfo struct {
 		ContactInfo struct {
 			CustomerNumber string `json:"customernumber"`
 			Contact        struct {
-				CompanyName string `json:"companyname"`
+				CompanyName      string `json:"companyname"`
+				ContactPerson    string `json:"contactperson"`
+				InvoiceReference string `json:"InvoiceReference"`
+				Address          string `json:"address"`
+				ZipCode          string `json:"zipcode"`
+				City             string `json:"city"`
+				Country          string `json:"country"`
+				NationalIDNumber string `json:"nationalidnumber"`
+				PhoneNumber      string `json:"phonenumber"`
+				Email            string `json:"email"`
 			} `json:"contact"`
+			Invoice struct {
+				Address string `json:"invoiceaddress"`
+				ZipCode string `json:"invoicezipcode"`
+				City    string `json:"invoicecity"`
+				Country string `json:"invoicecountry"`
+			} `json:"invoice"`
+			Settings struct {
+				SeparateInvoiceAddress string `json:"separateinvoiceaddress"`
+			} `json:"settings"`
 		} `json:"contactinfo"`
 		Debug ResponseDebug `json:"debug"`
 	} `json:"response"`
 }
 
-func (c *Client) CustomerContactInfo(args ...url.Values) (*CustomerContactInfo, error) {
-	req, err := c.PostRequest("customer/contactinfo", url.Values{})
+func (c *Client) CustomerContactInfo(args ...func(*url.Values)) (*CustomerContactInfo, error) {
+	data := &url.Values{}
+
+	for _, f := range args {
+		f(data)
+	}
+
+	req, err := c.PostRequest("customer/contactinfo", *data)
 	if err != nil {
 		return nil, err
 	}
