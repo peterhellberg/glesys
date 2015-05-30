fdee207596e12716aafa6286ba08911291c692ab
Peter Hellberg <peter@c7.se>
2015-05-30 18:38:17 +0200

    Early exit if GLESYS_API_KEY variable isn’t set

--- client_test.go
+++ client_test.go
@@ -15,6 +15,11 @@ func TestMain(m *testing.M) {
 	username = os.Getenv("GLESYS_USERNAME")
 	apiKey = os.Getenv("GLESYS_API_KEY")
 
+	if apiKey == "" {
+		print("GLESYS_API_KEY is not set\n")
+		os.Exit(1)
+	}
+
 	c = NewClient(username, apiKey, nil)
 
 	os.Exit(m.Run())
